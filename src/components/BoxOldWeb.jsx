45b390fb0eef5f74b0ed111ac15c9b21a610360e
farzafian <141146331+farzafian@users.noreply.github.com>
2023-12-14 22:27:41 +0700

    Update BoxOldWeb.jsx

--- src/components/BoxOldWeb.jsx
+++ src/components/BoxOldWeb.jsx
@@ -2,7 +2,7 @@
 const BoxOldWeb = () => {
 	return (
 		<div id="BoxOldWeb">
-			<a href="https://blog-xif7.farzafian.repl.co/">
+			<a href="https://open.spotify.com/playlist/0Ln4s5z9xtEm3RVe6ljRJQ?si=5698e8bb3dfe4256">
 				<div className="flex justify-between">
 					<img src="/link.png" alt="" className="w-auto h-6" />
 					<img src="/next.png" alt="" className="h-3 w-3" />
